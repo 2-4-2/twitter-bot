3962a89bdd79750d543b71cf69ce4f7d0f3afe4f
2-4-2 <68284149+2-4-2@users.noreply.github.com>
2026-02-11 21:44:43 +0300

    health.py

--- health.py
+++ health.py
@@ -1,1 +1,13 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+def run():
+    port = int(os.environ.get("PORT", "10000"))
+    HTTPServer(("0.0.0.0", port), Handler).serve_forever()
+  
